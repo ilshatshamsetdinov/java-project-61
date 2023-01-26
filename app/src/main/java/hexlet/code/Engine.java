8bb69d7bf5d10d63794510e3e3b428d0c07a115e
Ilshat Shamsetdinov <myheroisceleste@gmail.com>
2023-01-26 16:40:46 +0300

    fixed rewievs

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -12,40 +12,29 @@ public class Engine {
         return RandomUtils.nextInt(2, limit);
     }
     public static void launchGame(Game questionAndAnswer, Game task) {
-        greeting();
-        Scanner scana = new Scanner(System.in);
-        String userName = scana.next();
-        greetingEnd(userName);
-        var getTask = task.getTask();
-        System.out.println(getTask);
+        System.out.println("Welcome to the Brain Games! \nMay I have your name?");
+        Scanner scan = new Scanner(System.in);
+        String userName = scan.next();
+        System.out.println("Hello, " + userName + "! \n");
+        System.out.println(task.getTask());
         for (int i = 0; i < MAXROUND; i++) {
             var data = questionAndAnswer.getGameData();
             var question = data[0];
             var correctAnswer = data[1];
-            var askQuestion = "Question: " + question;
-            System.out.println(askQuestion);
-            Scanner scan = new Scanner(System.in);
+            System.out.println("Question: " + question);
             String answer = scan.next();
             System.out.println("Your answer: " + answer);
             if (answer.equals(correctAnswer)) {
                 System.out.println("Correct!");
-                if (i == 2) {
-                    System.out.println("Congratulations, " + userName + "!");
-                }
             } else {
                 System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n", answer, correctAnswer);
                 System.out.printf("Let's try again, %s!\n", userName);
                 break;
             }
+            if (i == 2) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
         }
     }
     private static final int MAXROUND = 3;
-    public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-    }
-    public static void greetingEnd(String userName) {
-        System.out.println("Hello, " + userName + "!");
-        System.out.println();
-    }
 }
